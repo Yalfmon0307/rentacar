ad13aad610721a987096d7821ca13ece7dc55324
Yalfmon0307 <montesyalfred0@gmail.com>
2025-09-07 14:58:20 -0600

    /

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/rentacar">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/alquilar" element={<Alquilar />} />
